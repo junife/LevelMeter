8db0c0dd774dddb53df24d922a7d173816c0bb5b
junife <junife.hu@gmail.com>
2016-03-27 22:16:09 +0800

    Creat project files for Level meter main board

--- LMMB_Debug/makedep.mk
+++ LMMB_Debug/makedep.mk
@@ -2,5 +2,5 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-LevelMeterMainboard_src\main.c
+LMMB_main\main.c
 
