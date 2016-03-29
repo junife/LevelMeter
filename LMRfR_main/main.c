6a0db0743cc935b6904c093893410c9aea395af2
junife <junife.hu@gmail.com>
2016-03-29 22:18:35 +0800

    Fix Timer compile error.
    Change PROGMEM and interrupt name, such as old name SIG_OVERFLOW0 change
    to TIMER0_OVF_vect

--- LMRfR_main/main.c
+++ LMRfR_main/main.c
@@ -1,7 +1,7 @@
 //*****************************************************************************
-// File Name	: rprintftest.c
+// File Name	: timertest.c
 // 
-// Title		: example usage of rprintf library functions
+// Title		: example usage of timer library functions
 // Revision		: 1.0
 // Notes		:	
 // Target MCU	: Atmel AVR series
@@ -10,22 +10,21 @@
 // Revision History:
 // When			Who			Description of change
 // -----------	-----------	-----------------------
-// 10-Sep-2002	pstang		Created the program
+// 30-Apr-2003	pstang		Created the program
 //*****************************************************************************
 
- 
+
 //----- Include Files ---------------------------------------------------------
-#include <avr/io.h>		// include I/O definitions (port names, pin names, etc)
+#include <avr/io.h>			// include I/O definitions (port names, pin names, etc)
 #include <avr/interrupt.h>	// include interrupt support
 
 #include "global.h"		// include our global settings
 #include "uart.h"		// include uart function library
 #include "rprintf.h"	// include printf function library
-//#include "timer.h"		// include timer function library (timing, PWM, etc)
-//#include "vt100.h"		// include VT100 terminal support
+#include "timerx8.h"		// include timer function library (timing, PWM, etc)
 
-void rprintfTest(void);
-//Hujunfeng
+void timerTest(void);
+
 //----- Begin Code ------------------------------------------------------------
 int main(void)
 {
@@ -34,128 +33,73 @@ int main(void)
 	uartInit();
 	// set the baud rate of the UART for our debug/reporting output
 	uartSetBaudRate(9600);
-	// initialize the timer system
-	//timerInit();
-
 	// initialize rprintf system
-	// - use uartSendByte as the output for all rprintf statements
-	//   this will cause all rprintf library functions to direct their
-	//   output to the uart
-	// - rprintf can be made to output to any device which takes characters.
-	//   You must write a function which takes an unsigned char as an argument
-	//   and then pass this to rprintfInit like this: rprintfInit(YOUR_FUNCTION);
 	rprintfInit(uartSendByte);
-
-	// initialize vt100 library
-	//vt100Init();
-	
-	// clear the terminal screen
-	//vt100ClearScreen();
 	
 	// run the test
-	rprintfTest();
+	timerTest();
 
 	return 0;
 }
 
-void rprintfTest(void)
+void timerTest(void)
 {
-	u16 val;
-	u08 mydata;
-	u08 mystring[10];
-	float b;
-	u08 small;
-	u16 medium;
-	u32 big;
-
 	// print a little intro message so we know things are working
-	rprintf("\r\nThis is my cool program!\r\n");
+	rprintf("\r\n\n\nWelcome to the timer library test program!\r\n");
 
-	
-	rprintf("\r\nWelcome to rprintf Test!\r\n");
+	// initialize the timer system
+	timerInit();
 
-	// print single characters
-	rprintfChar('H');
-	rprintfChar('e');
-	rprintfChar('l');
-	rprintfChar('l');
-	rprintfChar('o');
-	// print a constant string stored in FLASH
-	rprintfProgStrM(" World!");
-	// print a carriage return, line feed combination
-	rprintfCRLF();
-	// note that using rprintfCRLF() is more memory-efficient than
-	// using rprintf("\r\n"), especially if you do it repeatedly
+	// to use the internal timers to produce a calibrated delay,
+	// use the timerPause() function.  timerPause takes a 16-bit
+	// integer argument in milliseconds
 
-	mystring[0] = 'A';
-	mystring[1] = ' ';
-	mystring[2] = 'S';
-	mystring[3] = 't';
-	mystring[4] = 'r';
-	mystring[5] = 'i';
-	mystring[6] = 'n';
-	mystring[7] = 'g';
-	mystring[8] = '!';
-	mystring[9] = 0;	// null termination
-
-	// print a null-terminated string from RAM
-	rprintfStr(mystring);
-	rprintfCRLF();
-
-	// print a section of a string from RAM
-	// - start at index 2
-	// - print 6 characters
-	rprintfStrLen(mystring, 2, 6);
-	rprintfCRLF();
+	// example: wait for 1/2 of a second, or 500ms
+	rprintf("\r\nTest of timerPause() function\r\n");
+	rprintf("Here comes a 1/2-second delay...\r\n");
+	timerPause(500);
+	rprintf("Done!\r\n");
 
 
-	val = 24060;
-	mydata = 'L';
+	// here's an example of using the timer library to do
+	// pulse-width modulation or PWM.  PWM signals can be created on
+	// any output compare (OCx) pin.  See your processor's data sheet
+	// for more information on which I/O pins have output compare
+	// capability.
+	rprintf("\r\nTest of timer1 PWM output\r\n");
 
-	// print a decimal number
-	rprintf("This is a decimal number: %d\r\n", val);
+	// set the OC1x port pins to output
+	// We need to do this so we can see and use the PWM signal
+	// ** these settings are correct for most processors, but not for all
+	sbi(DDRD, PD4);
+	sbi(DDRD, PD5);
 
-	// print a hex number
-	rprintf("This is a hex number: %x\r\n", mydata);
-	
-	// print a character
-	rprintf("This is a character: %c\r\n", mydata);
+	// initialize timer1 for PWM output
+	// - you may use 8,9, or 10 bit PWM resolution
+	rprintf("Initializing timer1 for PWM\r\n");
+	timer1PWMInit(8);
 
-	// print hex numbers
-	small = 0x12;		// a char
-	medium = 0x1234;	// a short
-	big = 0x12345678;	// a long
+	// turn on the channel A PWM output of timer1
+	// - this signal will come out on the OC1A I/O pin
+	rprintf("Turning on timer1 channel A PWM output\r\n");
+	timer1PWMAOn();
 
-	rprintf("This is a 2-digit hex number (char) : ");
-	rprintfu08(small);
-	rprintfCRLF();
+	// set the duty cycle of the channel A output
+	// - let's try 25% duty, or 256*25% = 64
+	rprintf("Setting duty cycle to 25%%\r\n");
+	timer1PWMASet(64);
 
-	rprintf("This is a 4-digit hex number (short): ");
-	rprintfu16(medium);
-	rprintfCRLF();
+	// turn on channel B and set it to 75% duty cycle
+	rprintf("Turning on channel B too, with 75%% duty\r\n");
+	timer1PWMBOn();
+	timer1PWMBSet(192);
 
-	rprintf("This is a 8-digit hex number (long) : ");
-	rprintfu32(big);
-	rprintfCRLF();
+	// wait for 5 seconds
+	rprintf("Pause for 5 seconds...\r\n");
+	timerPause(5000);
 
-	// print a formatted decimal number
-	// - use base 10
-	// - use 8 characters
-	// - the number is signed [TRUE]
-	// - pad with '.' periods
-	rprintf("This is a formatted decimal number: ");
-	rprintfNum(10, 8, TRUE, '.', val);
-	rprintfCRLF();
-
-	b = 1.23456;
-
-	// print a floating point number
-	// use 10-digit precision
-	
-	// NOTE: TO USE rprintfFloat() YOU MUST ENABLE SUPPORT IN global.h
-	// use the following in your global.h: #define RPRINTF_FLOAT
-
-	//rprintf("This is a floating point number: ");
-	//rprintfFloat(8, b);
-	//rprintfCRLF();
+	// now turn off all PWM on timer1
+	rprintf("Turning off all PWM on timer1\r\n");
+	timer1PWMOff();
 }
+
