ef8735beb065c3774e0593ef3ae5b7e7c94b49b9
junife <junife.hu@gmail.com>
2016-04-14 12:05:43 +0800

    Level meter main board and remote board all work OK
    RS232 all OK, LCD display OK, push button for remote OK.

--- LMMB_Debug/makedep.mk
+++ LMMB_Debug/makedep.mk
@@ -6,8 +6,6 @@ common_avr-liberty-master_src\buffer.c
 
 common_avr-liberty-master_src\rprintf.c
 
-common_avr-liberty-master_src\timerx8.c
-
 common_avr-liberty-master_src\uart.c
 
 LMMB_main\appl.c
